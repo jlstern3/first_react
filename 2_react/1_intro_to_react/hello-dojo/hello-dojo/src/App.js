1fb443284b20e918c09d24168268a1628646a79f
Jessica Stern <jlstern3@gmail.com>
2021-05-12 13:08:13 -0400

    Hello-Dojo Assignment Complete

---
+++ 2_react/1_intro_to_react/hello-dojo/hello-dojo/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="title">Hello Dojo!</h1>
+        <h2 className="subtitle">Things I need to do:</h2>
+        <ul className="ul_list">
+          <li>Learn React</li>
+          <li>Climb Mt.Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
