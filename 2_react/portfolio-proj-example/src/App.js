3ae232583fab74fc401e5bf7999f1bf1d2855ee5
Jessica Stern <jlstern3@gmail.com>
2021-05-15 11:46:55 -0400

    Navbar working, files created for structure

---
+++ 2_react/portfolio-proj-example/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My Portfolio</h1>
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Main Content */}
+      {/* Projects */}
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_react/portfolio-proj-example/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'; // this is the React library that exists in the node_modules folder
+
+const Navbar = () => {
+    return(
+        <div>
+            <button>Home</button>
+            <button>Projects</button>
+            <button>About Me</button>
+            <button>Contact</button>
+        </div>
+    )
+}
+
+export default Navbar;
