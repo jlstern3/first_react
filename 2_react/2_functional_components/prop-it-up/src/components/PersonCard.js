7cf509e1766cf3573106ff2478f67e7cdfc43570
Jessica Stern <jlstern3@gmail.com>
2021-05-15 16:44:32 -0400

    Button increases birthday age by one when clicked

--- 2_react/2_functional_components/prop-it-up/src/components/PersonCard.js
+++ 2_react/2_functional_components/prop-it-up/src/components/PersonCard.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const PersonCard=(props) => {
-
+    const[birthdayAge, setBirthdayAge]=useState(props.age);
+    const handleClick = () => {
+        setBirthdayAge(birthdayAge+1);
+    }
     return(
         <div>
             <h1>{props.lastName}, {props.firstName}</h1>
-            <h3>Age: {props.age}</h3>
+            <h3>Age: {birthdayAge}</h3>
             <h3 className="hair-color">Hair Color: {props.hairColor}</h3>
+            <button onClick={handleClick}>Birthday Button for {props.firstName} {props.lastName}</button>
         </div>
     )
 }
