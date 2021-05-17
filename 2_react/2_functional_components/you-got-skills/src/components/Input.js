8267bb51c56b9412a4542d6c7510e638f91d224a
Jessica Stern <jlstern3@gmail.com>
2021-05-17 10:52:08 -0400

    Input clears when new  skill is added to array

--- 2_react/2_functional_components/you-got-skills/src/components/Input.js
+++ 2_react/2_functional_components/you-got-skills/src/components/Input.js
@@ -9,16 +9,27 @@ const Input = () => {
     const submitHandler=(e)=>{
         e.preventDefault();
         console.log("Adding new skill: " + newSkill);
-    // Saying: put newSkill into an array and put that array into skillList's state
-        setSkillList([...skillList, newSkill]);
+    // setter methods are ASYNCHRONOUS, they won't change the value in the getter until the entire function is done running!
+    // So ALWAYS use setters methods at the END of the function so you don't count on teh new value too soon
+    
+    // Saying: add new skill to the original array of skills
+        setSkillList([newSkill, ...skillList]);
+        // Reset input bar to empty string   so you don't have to keep clearing out data
+        setNewSkill("");
     }
     return (
         <div>
             <h3>Skills Form</h3>
             <form onSubmit={(e) => submitHandler(e)}>
-                <input type="text" name="newSkill" onChange={(e) => setNewSkill(e.target.value)} />
+                <input type="text" name="newSkill" value={newSkill} onChange={(e) => setNewSkill(e.target.value)} />
                 <button>Add Skill</button>
             </form>
+            {
+                // Add second parameter to map and make that the key of each p element to allow React to differentiate between p elements when manipulating the DOM
+                skillList.map((skill, index) => (
+                    <p key={index}>{ skill }</p>
+                ))
+            }
         </div>
     )
 }
