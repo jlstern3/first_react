3314df41ef8c42dcab22b08435579fc944f4883d
Jessica Stern <jlstern3@gmail.com>
2021-05-23 19:57:13 -0400

    Input box updates state array (but replaces old)

--- 2_react/2_functional_components/box-generator/src/App.js
+++ 2_react/2_functional_components/box-generator/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import React from 'react';
+import ColorForm from './components/ColorForm';
 
 function App() {
+
+
   return (
     <div className="App">
-      <h1>Hey Jess</h1>
+      <h1>Welcome to Box Generator!</h1>
+      <h3>Please add the color of the box you'd like to generate below.</h3>
+      <ColorForm />
     </div>
   );
 }
--- 2_react/2_functional_components/box-generator/src/components/ColorForm.js
+++ 2_react/2_functional_components/box-generator/src/components/ColorForm.js
@@ -1,1 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const ColorForm = () => {
+
+    // const createBox = (e)=>{
+    //     e.preventDefault();
+        // function to create new box w/ color value set to color's getter state key
+    //     console.log("You created a box");
+    // };
+    // const boxStyle = {
+    //     backgroundColor: {color},
+    //     margin: "15px",
+    //     width: "200px",
+    //     height: "200px",
+    // };
+    // setNewColor("");
+    const[newColor, setNewColor] = useState("");
+    const[boxList, setBoxList] = useState([]);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setBoxList([newColor]);
+    }
+
+    return(
+        <div>
+            <form onSubmit={(e) => submitHandler(e)}>
+                <label>Color</label>
+                <input 
+                    type="text" 
+                    name="newColor" 
+                    onChange = {(e) => setNewColor(e.target.value)} />
+                <button type="submit">Add</button>
+            </form>
+            {/* <div style={boxStyle}>Placeholder</div> */}
+        </div>
+    )
+}
+
+export default ColorForm;
