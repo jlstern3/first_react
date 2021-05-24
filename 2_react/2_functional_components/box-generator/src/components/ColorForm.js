f6368055fbf930503d4f9ff303751380dde3dfe8
Jessica Stern <jlstern3@gmail.com>
2021-05-24 17:49:50 -0400

    Assignment Complete

--- 2_react/2_functional_components/box-generator/src/components/ColorForm.js
+++ 2_react/2_functional_components/box-generator/src/components/ColorForm.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const ColorForm = (props) => {
-    const {boxList, setBoxList} = props;
-    const[newColor, setNewColor] = useState("");
-    const[integer, setInteger] = useState(200);
+    const { boxList, setBoxList } = props;
+    const [newColor, setNewColor] = useState("");
+    const [integer, setInteger] = useState(200);
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -15,28 +15,28 @@ const ColorForm = (props) => {
         setNewColor("");
         setInteger(200);
     }
-    
-    return(
+
+    return (
         <div>
-            <form onSubmit= {submitHandler}> 
+            <form onSubmit={submitHandler}>
                 <label>Color</label>
-                <input 
-                    type="text" 
-                    name="newColor" 
+                <input
+                    type="text"
+                    name="newColor"
                     value={newColor}
-                    onChange = {(e) => setNewColor(e.target.value)} />
+                    onChange={(e) => setNewColor(e.target.value)} />
                 <input
                     type="number"
                     name="integer"
-                    value = {integer}
-                    onChange = {(e) => setInteger(e.target.value)} />
+                    value={integer}
+                    onChange={(e) => setInteger(e.target.value)} />
 
                 <button type="submit">Add</button>
                 {
-                        newColor.length > 0 && newColor.length < 3 ?
-                        <p id = "error-message">Your color must be at least three characters long.</p>
+                    newColor.length > 0 && newColor.length < 3 ?
+                        <p id="error-message">Your color must be at least three characters long.</p>
                         : null
-                    }
+                }
             </form>
 
         </div>
