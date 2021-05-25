30d9b5cbb2d7455c877f5536b96ee263f97dd276
Jessica Stern <jlstern3@gmail.com>
2021-05-24 20:31:34 -0400

    Display.js updated

--- 2_react/2_functional_components/tabs/tabs/src/App.js
+++ 2_react/2_functional_components/tabs/tabs/src/App.js
@@ -9,6 +9,8 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Welcome to the Tabs Assignment!</h1>
+      <h3>Please enter the name of your tag and the content you'd like displayed</h3>
       <Tabs tabsArray = {tabsArray} setTabsArray = {setTabsArray} />
       <Display tabsArray = {tabsArray} />
     </div>
--- 2_react/2_functional_components/tabs/tabs/src/components/Display.js
+++ 2_react/2_functional_components/tabs/tabs/src/components/Display.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
-const Display = () => {
-    return(
-        <h1>Hi Players</h1>
+const Display = (props) => {
+    const { tabsArray } = props;
+
+    return (
+        <div>
+            {
+                tabsArray.map((tab, index) => (
+                    <div key={index} style={{
+                        margin: "15px",
+                        display: "inline-block",
+                        overflow: "scroll",
+                    }}>{tab.tab}</div>
+                    
+                ))
+            }
+        </div>
     );
+
 }
+//     return (
+//         <div>
+//             {
+//                 tabsArray.map((tab, index) => (
+//                     <p key={index} style={{
+//                         margin: "20px",
+//                         color: "red",
+//                         }}>tab.tab</p>
+            
+//                     <p key={index} style={{ 
+//                         margin: "20px", 
+//                         display: inline-block,}}>{tab.content}
+//                     </p>
+//                 ))
+//             }
+//         </div>
+//     );
+
+// }
 
 export default Display;
