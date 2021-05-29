0cecc4cf3df0df091b0ab2cfb22681041dd7e073
Jessica Stern <jlstern3@gmail.com>
2021-05-29 09:42:35 -0400

    Need to correct syntax

--- 2_react/3_apis/using-apis/src/App.js
+++ 2_react/3_apis/using-apis/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+  const[luke, setLuke] = useState({});
+
+  const getLukeSkywalker = () =>{
+    fetch("https://swapi.dev/api/people/1/")
+    .then((response) => {
+      console.log(response);
+      response.json()
+          .then(jsonResponse) => {
+            console.log(jsonResponse);
+          })
+          .catch((jsonError) => {
+            console.log(jsonError);
+          });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    console.log("Let's relax while we wait for swapi to return data!");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={(e)=>getLukeSkywalker()}>Get Luke Skywalker</button>
+      <hr />
+      <p>Name: {luke.name}</p>
     </div>
   );
 }
