9ad57a7383713a3863da3372955818e1a1904bce
Jessica Stern <jlstern3@gmail.com>
2021-05-30 16:36:01 -0400

    Starships grabbed from API

--- 2_react/3_apis/using-apis/src/App.js
+++ 2_react/3_apis/using-apis/src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 
 function App() {
   const[luke, setLuke] = useState({});
+  const [starships, setStarships] = useState([]);
 
   const getLukeSkywalker = () => {
     fetch("https://swapi.dev/api/people/1/")
@@ -23,14 +24,42 @@ function App() {
     console.log("Let's relax while we wait for swapi to return data!");
   }
 
+  const getStarships = () => 
+    fetch("https://swapi.dev/api/starships")
+    .then((response) => {
+      response.json()
+          .then((jsonResponse) => {
+            setLuke(jsonResponse);
+            setStarships(jsonResponse.results);
+          })
+          .catch((jsonError) => {
+            console.log(jsonError);
+          });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    console.log("Let's relax while we wait for swapi to return data!");
+  }
+
   return (
     <div className="App">
       <button onClick={(e)=>getLukeSkywalker()}>Get Luke Skywalker</button>
+      <button onClick={(e)=>getStarships()}>Get Starships</button>
+
       <hr />
       <p>Name: {luke.name}</p>
       <p>Height: { luke.height }</p>
+      {
+        starships.map((ship, index) => (
+          <div>
+            <h4 key={index}>{ship.name}</h4>
+            <p>Number of Passengers: {ship.passengers}</p>
+          </div>
+        ))     
+      }
     </div>
   );
-}
+
 
 export default App;
