afdfac2755e41b6d955677149eae9f467d7400da
Jessica Stern <jlstern3@gmail.com>
2021-06-02 11:44:43 -0400

    Base setup for project completion

--- 2_react/2_functional_components/coin-flipping/src/App.js
+++ 2_react/2_functional_components/coin-flipping/src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
 
 function App() {
+
+  function fiveHeads(){
+    return new Promise ((resolve, reject) => {
+      // my code here
+    });
+  }
+
+  fiveHeads()
+    .then( res => console.log(res))
+    .catch( err => console.log(err));
+  console.log("When does this run now?");
+  
   return (
     <div className="App">
       <h2>Hello World</h2>
