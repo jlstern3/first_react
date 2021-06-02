a83b333601fbb864f388fb889a425e7d108fb252
Jessica Stern <jlstern3@gmail.com>
2021-06-02 19:15:17 -0400

    Formatting and programming complete

--- 2_react/3_apis/pokemon-api/src/App.js
+++ 2_react/3_apis/pokemon-api/src/App.js
@@ -1,32 +1,35 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [pokemon, setPokemon] = useState([])
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
-    .then(response => {
-      return response.json()
-    })
-    .then(response => {
-      setPokemon(response.results)
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+      .then(response => {
+        return response.json()
+      })
+      .then(response => {
+        setPokemon(response.results)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }, []);
-  
+
   return (
     <div className="App">
-      {
-        pokemon.map((character, index) => (
-          <ul>
-            <li key={index}>{character.name}</li>
-          </ul>
-        ))
-      }
-      
+      <h1>108 Pokemon, Named!</h1>
+      <div className="names">
+        {
+          pokemon.map((character, index) => (
+            <ul>
+              <li key={index}>{character.name}</li>
+            </ul>
+          ))
+        }
+      </div>
+
     </div>
   );
 }
