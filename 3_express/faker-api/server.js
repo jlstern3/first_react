4437b2030c186b5fba970256773f122f249e6e3d
Jessica Stern <jlstern3@gmail.com>
2021-06-07 17:17:55 -0400

    3 routes = create/return new fake user and company

--- 3_express/faker-api/server.js
+++ 3_express/faker-api/server.js
@@ -1,25 +1,75 @@
+//import necessary libraries
 const express = require('express');
 const app = express();
 const faker = require('faker');
 
 //configure server
 app.use(express.json());
-app.use(urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
+
+//USER
+const createUser = () =>{
+    const newUser={
+        id: faker.datatype.number(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        phoneNumber: faker.phone.phoneNumber(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+    };
+    return newUser;
+};
+
+
+//COMPANY
+const createCompany = () => {
+    const newCompany = {
+        id: faker.datatype.number(),
+        name: faker.company.companyName(),
+        address: [{
+            street: faker.address.streetName(),
+            city: faker.address.cityName(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country(),
+        }],
+    }
+    return newCompany;
+}
+
 
 //routes need an http verb, a path, and a function with two parameters (request, response) to run 
 //must return something 
-// we can create a function to return a random / fake "Product"
-const createProduct = () => {
-    const newFake = {
-        name: faker.commerce.productName(),
-        price: ' + faker.commerce.price(),
-        department: faker.commerce.department()
-    };
-    return newFake;
-};
-    
-const newFakeProduct = createProduct();
-console.log(newFakeProduct);
+app.get("/api/users/new", (request,response)=>{
+    const newUserCreated = createUser();
+    console.log(newUserCreated);
+
+    response.json({
+        message: "We went out and grabbed a new user for you",
+        newUser: newUserCreated
+    })
+})
+
+app.get("/api/companies/new", (request,response) => {
+    const newCompanyCreated = createCompany();
+    console.log(newCompanyCreated);
+
+    response.json({
+        message: "We created a new company for you!",
+        newCompany: newCompanyCreated,
+    })
+})
+
+app.get("/api/user/company", (request, response) => {
+    const newUserCreated = createUser();
+    const newCompanyCreated = createCompany();
+    console.log("We're doubling down on our efforts!");
+    response.json({
+        message: "Doing all the dirty work today",
+        newCompany: newCompanyCreated,
+        newUser: newUserCreated,
+    })
+})
 
 
 //activate server on a port
