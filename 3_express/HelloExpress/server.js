9c991c808484626ff705dd0b6e21a60f19beef29
Jessica Stern <jlstern3@gmail.com>
2021-06-07 11:08:22 -0400

    Server configured for json and url encoding

--- 3_express/HelloExpress/server.js
+++ 3_express/HelloExpress/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 
+//confingure server here
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//create routes and functions here 
 
 app.get("/api", (request, response) => {
   response.send("Let's see nodemon in action!");
 });
 
+
+//tell server to listen on port 8000
 const server = app.listen(8000, () =>
   console.log("Listening on port 8000")
 );
