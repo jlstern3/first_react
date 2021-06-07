5e48a5ad50e7269e309ae4d6f49951f9517772a7
Jessica Stern <jlstern3@gmail.com>
2021-06-07 11:05:57 -0400

    Functioning correctly with nodemon

--- 3_express/HelloExpress/server.js
+++ 3_express/HelloExpress/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 
-// req is short for request
-// res is short for response
-app.get("/api", (req, res) => {
-  res.send("Our express api server is now sending this over to the browser");
+
+app.get("/api", (request, response) => {
+  response.send("Let's see nodemon in action!");
 });
 
 const server = app.listen(8000, () =>
-  console.log(`Server is locked and loaded on port ${server.address().port}!`)
+  console.log("Listening on port 8000")
 );
+
+
