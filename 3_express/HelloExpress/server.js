5483ca1ecb11a7fef2f58511eb72962ce549fd81
Jessica Stern <jlstern3@gmail.com>
2021-06-07 11:15:12 -0400

    Users hard coded

--- 3_express/HelloExpress/server.js
+++ 3_express/HelloExpress/server.js
@@ -1,20 +1,40 @@
 const express = require("express");
 const app = express();
 
-//confingure server here
+//confingure server here (Express middleware functions)
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+
 //create routes and functions here 
+// we can hard code some users like this
+// later on we will want to store users in a database
+const users = [
+  { firstName: "Reimu",  lastName: "Hakurei"    },
+  { firstName: "Marisa", lastName: "Kirisame"   },
+  { firstName: "Sanae",  lastName: "Kochiya"    },
+  { firstName: "Sakuya", lastName: "Izayoi"     },
+  { firstName: "Momiji", lastName: "Inubashiri" }
+];
+  
+app.get("/api/users", (req, res) => {
+    res.json( users );
+});
 
 app.get("/api", (request, response) => {
   response.send("Let's see nodemon in action!");
 });
 
+app.post("/api/users", (request, response) => {
+  console.log(request.body);
+  users.push(request.body);
+  //ALWAYS NEED TO RESPOND WITH SOMETHING
+  response.json({status: "ok"});
+})
 
 //tell server to listen on port 8000
 const server = app.listen(8000, () =>
-  console.log("Listening on port 8000")
+  console.log("The server is listening on port 8000")
 );
 
 
