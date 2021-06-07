618677ebef3a274d6ae3778eea517d599dcf7dda
Jessica Stern <jlstern3@gmail.com>
2021-06-07 16:47:50 -0400

    Faker API installed

--- 3_express/faker-api/server.js
+++ 3_express/faker-api/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const faker = require('faker');
 
 //configure server
-// app.use(express.json());
-// app.use(urlencoded({extended: true}));
+app.use(express.json());
+app.use(urlencoded({extended: true}));
 
 //routes need an http verb, a path, and a function with two parameters (request, response) to run 
 //must return something 
+// we can create a function to return a random / fake "Product"
+const createProduct = () => {
+    const newFake = {
+        name: faker.commerce.productName(),
+        price: ' + faker.commerce.price(),
+        department: faker.commerce.department()
+    };
+    return newFake;
+};
+    
+const newFakeProduct = createProduct();
+console.log(newFakeProduct);
+
 
 //activate server on a port
 app.listen(8000, ()=>{
