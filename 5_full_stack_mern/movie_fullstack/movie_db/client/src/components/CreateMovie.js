a4970db1b84a59887d47073719465a491c0be21a
Jessica Stern <jlstern3@gmail.com>
2021-06-18 19:37:31 -0400

    MovieForm created, props passed through

--- 5_full_stack_mern/movie_fullstack/movie_db/client/src/components/CreateMovie.js
+++ 5_full_stack_mern/movie_fullstack/movie_db/client/src/components/CreateMovie.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'; 
 import {Link, navigate} from '@reach/router';
+import MovieForm from './MovieForm';
 
 const CreateMovie = (props) =>{
 
     // create state for the new movie Object
-    const [movie, setAllMovie] = useState ({
+    const [movie, setMovie] = useState ({
         "title": "",
         "genre": "",
         "producer": "",
@@ -14,8 +15,7 @@ const CreateMovie = (props) =>{
         "rating": "",
         "isOnNetflix": false,
         "coverArtURL": ""
-
-    })
+    });
     const[errors, setErrors] = useState({});
 
     // handle form submit to create the document through the API
@@ -23,7 +23,6 @@ const CreateMovie = (props) =>{
         e.preventDefault();
         // call axios to post object to API
         axios.post("http://localhost:8000/api/movies", movie) 
-        
             .then((res) => {
                 console.log(res.data);
                 // if we have validation errors, not errors with our server
@@ -32,196 +31,23 @@ const CreateMovie = (props) =>{
                 }
                 else{
                 // on success, redirect (navigate) to the movie list
-
                     navigate("/movies");
                 }
-
             })
             // failure, save the errors in state so the user can correct 
-
             .catch(err => {
                 console.log(err)
             })
-}
-
-
-    const inputChange = (e) => {
-        console.log("Input name: " + e.target.name);
-        console.log("input value: " + e.target.value);
-        console.log("input checked: " + e.target.checked);
-
-    // create a new movie object and create a copy of what's currently in state
-
-        let newMovieObject = { ...movie};
-    // make a change to the copy
-        if(e.target.name === "isOnNetflix"){
-            newMovieObject[e.target.name] = e.target.checked;
-        }
-        else{
-            newMovieObject[e.target.name] = e.target.value;
-        }
-    // set the changes in state
-        setAllMovie(newMovieObject);
-    }
-
-    const genres = [
-        'Action',
-        'Adventure',
-        'Animation',
-        'Cinema Verite',
-        'Comedy',
-        'Drama',
-        'Experimental',
-        'Family',
-        'Fantasy',
-        'Foreign',
-        'Horror',
-        'Kung Fu',
-        'Musical',
-        'Mystery',
-        'Romance',
-        'Sci-Fi',
-    ];
-
-    const rating = [
-        'G',
-        'PG',
-        'PG-13',
-        'R',
-        'NR'
-    ]
-
+} 
     return(
         <div>
             <h1>Create Movie</h1>
-            <form onSubmit = {(e) => handleSubmit(e)}>
-                <div>
-                    <label>Title: </label>
-                    <input type = "text" 
-                    name = "title" 
-                    value = {movie.title} 
-                    onChange = {(e) => inputChange(e)}/>
-                    {
-                        errors.title ?
-                        <span className = "errors">{errors.title.message}</span>
-                        : null
-                    }
-                </div>
-                <div>
-                    <label>Genre</label>
-                    <select 
-                    name = "genre"
-                    value = {movie.genre}
-                    onChange = {(e) => inputChange(e)}>
-                        {/* blank first option line in dropdown */}
-                    <option value=""></option>
-                    
-                    {
-                        genres.map((movieGenre, index) => (
-                            <option value = {movieGenre} key = {movieGenre}>{movieGenre}</option>
-                        ))
-                    }
-                    </select>  
-                    {
-                        errors.genre ?
-                        <span className = "errors">{errors.genre.message}</span>
-                        : null
-                    }                
-                </div>
-                <div>
-                    <label>Producer</label>
-                    <input type = "text" 
-                    name = "producer" 
-                    value = {movie.producer} 
-                    onChange = {(e) => inputChange(e)}/>   
-                    {
-                        errors.producer ?
-                        <span className = "errors">{errors.producer.message}</span>
-                        : null
-                    }
-                </div>
-                <div>
-                    <label>Movie Length (mins)</label>
-                    <input type = "number" 
-                    min = "30"
-                    name = "length" 
-                    value = {movie.length} 
-                    onChange = {(e) => inputChange(e)}/>   
-                    {
-                        errors.length ?
-                        <span className = "errors">{errors.length.message}</span>
-                        : null
-                    }
-                </div>
-                <div>
-                    <label>Production Date</label>
-                    <input type = "text" 
-                    name = "dateProduction" 
-                    value = {movie.dateProduction} 
-                    onChange = {(e) => inputChange(e)}/>   
-                    {
-                        errors.dateProduction ?
-                        <span className = "errors">{errors.dateProduction.message}</span>
-                        : null
-                    }
-                </div>
-                <div>
-                    <label>Ratings</label>
-                    <select 
-                    name = "rating"
-                    value = {movie.rating}
-                    onChange = {(e) => inputChange(e)}>
-                        {/* blank first option line in dropdown */}
-                    <option value=""></option>
-                    
-                    {
-                        rating.map((movieRating, index) => (
-                            <option value = {movieRating} key = {movieRating}>{movieRating}</option>
-                        ))
-                    }
-                    </select>  
-                    {
-                        errors.rating ?
-                        <span className = "errors">{errors.rating.message}</span>
-                        : null
-                    }   
-                </div>
-                <div className="checkboxDiv">
-                    <input
-                    type="checkbox"
-                    name="isOnNetflix"
-                    checked = {movie.isOnNetflix}
-                    onChange = {(e) => inputChange(e)}/>
-                    <label>Movie is on Netflix</label>
-                    {
-                        errors.isOnNetflix ?
-                        <span className = "errors">{errors.isOnNetflix.message}</span>
-                        : null
-                    } 
-                </div>
-                <div>
-                    <label>Cover Art URL</label>
-                    <input
-                    type="text"
-                    name="coverArtURL"
-                    value = {movie.coverArtURL}
-                    onChange = {(e) => inputChange(e)}/>
-                    {
-                        errors.coverArtURL ?
-                        <span className = "errors">{errors.coverArtURL.message}</span>
-                        : null
-                    }
-                </div>
-
-
-                {/* labels for inputs */}
-                {/* input that update state as they changes
-                //values int he inputs that use state as the valeus */}
-                {/* validate/display errors that come from the backend server */}
-                <button type="submit">Create a Movie</button>
-                {/* return to all movies butt/link */}
-                <button onClick = {() => navigate("/movies")}>Cancel</button>
-            </form>
+            <MovieForm
+            movie = {movie}
+            setMovie = {setMovie}
+            errors = {errors}
+            handleSubmit = {handleSubmit}
+            />
         </div>
     )
 }
