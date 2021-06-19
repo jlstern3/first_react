3a11430826f9fa232b607d184d175ec9343e5bcd
Jessica Stern <jlstern3@gmail.com>
2021-06-19 13:50:24 -0400

    Can grab movie and set into state (UpdateMovie.js)

--- 5_full_stack_mern/movie_fullstack/movie_db/client/src/components/UpdateMovie.js
+++ 5_full_stack_mern/movie_fullstack/movie_db/client/src/components/UpdateMovie.js
@@ -1,12 +1,55 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'; 
 import {Link, navigate} from '@reach/router';
+import MovieForm from './MovieForm';
 
 const UpdateMovie = (props) =>{
+    const {id} = props
+    // create state for the new movie Object
+    const [movie, setMovie] = useState ({});
+    const[errors, setErrors] = useState({});
 
+    useEffect(()=> {
+        axios.get("http://localhost:8000/api/movies/" + id)
+        .then((res) => {
+            console.log(res.data);
+            setMovie(res.data);
+        })
+        .catch(err => { 
+            console.log(err); 
+        })
+    }, []);
+
+    // handle form submit to create the document through the API
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // call axios to post object to API
+        axios.post("http://localhost:8000/api/movies", movie) 
+            .then((res) => {
+                console.log(res.data);
+                // if we have validation errors, not errors with our server
+                if(res.data.errors){
+                    setErrors(res.data.errors)
+                }
+                else{
+                // on success, redirect (navigate) to the movie list
+                    navigate("/movies");
+                }
+            })
+            // failure, save the errors in state so the user can correct 
+            .catch(err => {
+                console.log(err)
+            })
+} 
     return(
         <div>
             <h1>Update Movie</h1>
+            <MovieForm
+            movie = {movie}
+            setMovie = {setMovie}
+            errors = {errors}
+            handleSubmit = {handleSubmit}
+            />
         </div>
     )
 }
--- 5_full_stack_mern/movie_fullstack/movie_db/server/config/mongoose.config.js
+++ 5_full_stack_mern/movie_fullstack/movie_db/server/config/mongoose.config.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 const dbName = "movies-db";
 
+// Connect to the database (mongodb)
 mongoose.connect('mongodb://localhost/' + dbName, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
+//On server  success
     .then(() => console.log(`Established a connection to the "${dbName}" database`))
+//On server error
     .catch( err => console.log(`Error establishing "${dbName}" connection`, err));
 
