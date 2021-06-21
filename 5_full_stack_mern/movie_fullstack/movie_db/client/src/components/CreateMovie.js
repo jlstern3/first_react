13f9b144712d8a6a88d35ed9c566ce64e4031e75
Jessica Stern <jlstern3@gmail.com>
2021-06-21 13:06:10 -0400

    Formatted component

--- 5_full_stack_mern/movie_fullstack/movie_db/client/src/components/CreateMovie.js
+++ 5_full_stack_mern/movie_fullstack/movie_db/client/src/components/CreateMovie.js
@@ -1,13 +1,13 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'; 
-import {Link, navigate} from '@reach/router';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, navigate } from '@reach/router';
 import MovieForm from './MovieForm';
 import DeleteMovie from './DeleteMovie';
 
-const CreateMovie = (props) =>{
+const CreateMovie = (props) => {
 
     // create state for the new movie Object
-    const [movie, setMovie] = useState ({
+    const [movie, setMovie] = useState({
         "title": "",
         "genre": "",
         "producer": "",
@@ -17,21 +17,21 @@ const CreateMovie = (props) =>{
         "isOnNetflix": false,
         "coverArtURL": ""
     });
-    const[errors, setErrors] = useState({});
+    const [errors, setErrors] = useState({});
 
     // handle form submit to create the document through the API
     const handleSubmit = (e) => {
         e.preventDefault();
         // call axios to post object to API
-        axios.post("http://localhost:8000/api/movies", movie) 
+        axios.post("http://localhost:8000/api/movies", movie)
             .then((res) => {
                 console.log(res.data);
                 // if we have validation errors, not errors with our server
-                if(res.data.errors){
+                if (res.data.errors) {
                     setErrors(res.data.errors)
                 }
-                else{
-                // on success, redirect (navigate) to the movie list
+                else {
+                    // on success, redirect (navigate) to the movie list
                     navigate("/movies");
                 }
             })
@@ -39,16 +39,16 @@ const CreateMovie = (props) =>{
             .catch(err => {
                 console.log(err)
             })
-} 
-    return(
+    }
+    return (
         <div>
             <h1>Create Movie</h1>
             <MovieForm
-            movie = {movie}
-            setMovie = {setMovie}
-            errors = {errors}
-            handleSubmit = {handleSubmit}
-            submitButtonLabel = {"Create Movie"} 
+                movie={movie}
+                setMovie={setMovie}
+                errors={errors}
+                handleSubmit={handleSubmit}
+                submitButtonLabel={"Create Movie"}
             />
         </div>
     )
