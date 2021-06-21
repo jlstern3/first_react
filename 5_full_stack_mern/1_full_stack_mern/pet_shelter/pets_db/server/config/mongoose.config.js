57513e874baee770040bc9e57e0c9127780bbe96
Jessica Stern <jlstern3@gmail.com>
2021-06-20 20:07:52 -0400

    Modularized folder structure created

---
+++ 5_full_stack_mern/1_full_stack_mern/pet_shelter/pets_db/server/config/mongoose.config.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const dbName = "pets_db";
+
+// Connect to the database (mongodb)
+mongoose.connect('mongodb://localhost/' + dbName, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+//On server  success
+    .then(() => console.log(`Established a connection to the "${dbName}" database`))
+//On server error
+    .catch( err => console.log(`Error establishing "${dbName}" connection`, err));
+
